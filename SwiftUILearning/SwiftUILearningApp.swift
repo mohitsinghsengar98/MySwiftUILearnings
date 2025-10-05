a52f65e65d23eb9706704111b0dc8ea1ec65a306
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-10-05 22:18:56 +0530

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Mohit Sengar on 05/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
